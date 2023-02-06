cc7d38a9667f8f76b3acf61a8917621405bea6f2
Aleksandra Tutueva <avtutueva@gmail.com>
2023-02-06 16:10:23 +0100

    goroutines testing

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"example.com/mainFile/painter_calculator"
+	"fmt"
+	"time"
 )
 
+func sum(nums []int, ch chan<- int) {
+	result := 0
+	for _, num := range nums {
+		result += num
+		time.Sleep(time.Second)
+		fmt.Println(num, result)
+	}
+	ch <- result
+}
+
 func main() {
-	painter_calculator.Scenario()
+	numbers := []int{1, -1, 2, 3, 10, 5, 4, -4, 12, 0, 7}
+	ch := make(chan int, 2)
+
+	go sum(numbers[:len(numbers)/2], ch)
+	go sum(numbers[len(numbers)/2:], ch)
+
+	fmt.Println(<-ch + <-ch)
+
 }
